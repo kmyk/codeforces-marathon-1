dec3f7246b6dee56344ac2fd0a51d7675c92c53a
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-06-13 02:31:35 +0900

    区間に分けて山登り score = 350 ぐらい

--- a.cpp
+++ a.cpp
@@ -30,18 +30,85 @@ template <class T> istream & operator >> (istream & in, vector<T> & a) { for (T 
 template <class T> ostream & operator << (ostream & out, vector<T> const & a) { bool i = false; for (T const & it : a) { if (i) out << ' '; else i = true; out << it; } return out; }
 
 
-const int n = 5000;
-const int k = 2000;
-const int x = 100;
-int main() {
-    random_device device;
-    default_random_engine engine(device());
+vector<bool> random_binary(int n) {
+    static random_device device;
+    static default_random_engine engine(device());
     uniform_int_distribution<int> dist(0, 1);
-    repeat (i,x) {
-        string s; repeat (j,n) s += '0' + dist(engine);
-        cout << s << endl;
+    vector<bool> xs(n); repeat (i,n) xs[i] = dist(engine);
+    return xs;
+}
+string decode(vector<bool> const & xs) {
+    int n = xs.size();
+    string s(n, '\0');
+    repeat (i,n) s[i] = '0' + xs[i];
+    return s;
+}
+vector<bool> encode(string const & s) {
+    int n = s.length();
+    vector<bool> xs(n);
+    repeat (i,n) xs[i] = s[i] - '0';
+    return xs;
+}
+string to_hexadecimal(vector<bool> const & xs) {
+    const char *table = "0123456789ABCDEF";
+    int n = xs.size();
+    assert (n % 4 == 0);
+    string s;
+    repeat (i,n/4) {
+        int y = 0;
+        repeat (j,4) {
+            y <<= 1;
+            if (xs[i*4+j]) y |= 1;
+        }
+        s += table[y];
+    }
+    return s;
+}
+string to_hashed(vector<bool> const & xs, int l) {
+    int n = xs.size();
+    string s = decode(xs);
+    string t;
+    repeat (i,n/l) {
+        uint64_t y = hash<string>()(s.substr(i*l, l));
+        char buf[8]; snprintf(buf, 8, "%8llx", y);
+        t += buf;
+    }
+    return t;
+}
+vector<bool> concat(vector<vector<bool> > const & xss) {
+    string s;
+    for (auto xs : xss) s += decode(xs);
+    return encode(s);
+}
+const int N = 5000;
+const int K = 2000;
+const int X = 100;
+int main() {
+    map<vector<bool>,int> history;
+    auto query = [&](vector<bool> const & xs) {
+        cout << decode(xs) << endl;
         cout.flush();
         int score; cin >> score;
+        cerr << "\"" << to_hashed(xs, 250) << "\" -> " << score << endl;
+        cerr.flush();
+        history[xs] = score;
+        return score;
+    };
+    const int l = 200;
+    vector<vector<bool> > xss(N/l);
+    repeat (i,N/l) xss[i] = random_binary(l);
+    repeat (i,X) {
+        if (i == 0) {
+            query(concat(xss));
+        } else {
+            int j = i / (X/(N/l));
+            vector<vector<bool> > yss = xss;
+            yss[j] = random_binary(l);
+            int score = query(concat(yss));
+            if (history[concat(xss)] < score) {
+                xss[j] = yss[j];
+            }
+        }
     }
     return 0;
 }
