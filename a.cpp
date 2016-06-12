4e52f91c1b6a2b9fa0569c0534b256009acf33b5
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-06-13 01:41:25 +0900

    愚直解 ランダムにし続けるだけ score = 150 程度

---
+++ a.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <set>
+#include <map>
+#include <queue>
+#include <tuple>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <random>
+#include <cstdio>
+#include <cassert>
+#define repeat(i,n) for (int i = 0; (i) < (n); ++(i))
+#define repeat_from(i,m,n) for (int i = (m); (i) < (n); ++(i))
+#define repeat_reverse(i,n) for (int i = (n)-1; (i) >= 0; --(i))
+#define repeat_from_reverse(i,m,n) for (int i = (n)-1; (i) >= (m); --(i))
+#define whole(f,x,...) ([&](decltype((x)) y) { return (f)(begin(y), end(y), ## __VA_ARGS__); })(x)
+#define dump(x)  cerr << #x << " = " << (x) << endl
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl
+typedef long long ll;
+template <class T> bool setmax(T & l, T const & r) { if (not (l < r)) return false; l = r; return true; }
+template <class T> bool setmin(T & l, T const & r) { if (not (r < l)) return false; l = r; return true; }
+template <typename F> auto compare_with(F const & f) { return [=](auto x, auto y) { return f(x) < f(y); }; }
+using namespace std;
+template <typename T, typename X> auto vectors(T a, X x) { return vector<T>(x, a); }
+template <typename T, typename X, typename Y, typename... Zs> auto vectors(T a, X x, Y y, Zs... zs) { auto cont = vectors(a, y, zs...); return vector<decltype(cont)>(x, cont); }
+template <class T> istream & operator >> (istream & in, vector<T> & a) { for (T & it : a) in >> it; return in; }
+template <class T> ostream & operator << (ostream & out, vector<T> const & a) { bool i = false; for (T const & it : a) { if (i) out << ' '; else i = true; out << it; } return out; }
+
+
+const int n = 5000;
+const int k = 2000;
+const int x = 100;
+int main() {
+    random_device device;
+    default_random_engine engine(device());
+    uniform_int_distribution<int> dist(0, 1);
+    repeat (i,x) {
+        string s; repeat (j,n) s += '0' + dist(engine);
+        cout << s << endl;
+        cout.flush();
+        int score; cin >> score;
+    }
+    return 0;
+}
