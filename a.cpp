dfce2ae0c8fb21e81ec8d380f7320999819d60fd
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-06-13 19:15:57 +0900

    初期値ガチャ実装 score = 380 ぐらいで微妙に良化

--- a.cpp
+++ a.cpp
@@ -75,28 +75,86 @@ vector<bool> concat(vector<vector<bool> > const & xss) {
     for (auto xs : xss) s += decode(xs);
     return encode(s);
 }
+vector<vector<bool> > unconcat(vector<bool> const & xs, int l) {
+    int n = xs.size();
+    assert (n % l == 0);
+    vector<vector<bool> > yss(n/l, vector<bool>(l));
+    repeat (i,n) yss[i/l][i%l] = xs[i];
+    return yss;
+}
+vector<bool> flip(vector<bool> const & xs) {
+    int n = xs.size();
+    vector<bool> ys(n);
+    repeat (i,n) ys[i] = not xs[i];
+    return ys;
+}
+template <typename F>
+vector<bool> select_best_with(map<vector<bool>,int> const & history, F f) {
+    int score = -1;
+    vector<bool> xs;
+    for (auto it : history) {
+        vector<bool> ys; int yscore; tie(ys, yscore) = f(it.first, it.second);
+        if (score < yscore) {
+            score = yscore;
+            xs = ys;
+        }
+    }
+    return xs;
+}
+vector<bool> select_best(map<vector<bool>,int> const & history) {
+    return select_best_with(history, [](vector<bool> const & ys, int score) {
+        return make_pair(ys, score);
+    });
+}
+
 const int N = 5000;
 const int K = 2000;
 const int X = 100;
+const int ES = N - 2*K;
+
+int flipped_score(int score) {
+    return ES + (ES - score);
+}
+vector<bool> select_flipped_best(map<vector<bool>,int> const & history) {
+    return select_best_with(history, [](vector<bool> ys, int score) {
+        ys = flip(ys);
+        score = flipped_score(score);
+        return make_pair(ys, score);
+    });
+}
+
 int main() {
     map<vector<bool>,int> history;
+    int query_count = 0;
     auto query = [&](vector<bool> const & xs) {
+        query_count += 1;
         cout << decode(xs) << endl;
         cout.flush();
         int score; cin >> score;
-        cerr << "\"" << to_hashed(xs, 250) << "\" -> " << score << endl;
+        cerr << "query " << query_count << ":\t\"" << to_hashed(xs, 250) << "\" -> " << score << endl;
         cerr.flush();
         history[xs] = score;
         return score;
     };
     const int l = 200;
+    const int x0 = 10;
     vector<vector<bool> > xss(N/l);
-    repeat (i,N/l) xss[i] = random_binary(l);
-    repeat (i,X) {
-        if (i == 0) {
+    while (query_count < X) {
+        if (query_count < x0) {
+            repeat (i,N/l) xss[i] = random_binary(l);
             query(concat(xss));
+            if (query_count == x0) {
+                vector<bool> ys = select_best(history);
+                vector<bool> zs = select_flipped_best(history);
+                if (history[ys] < flipped_score(history[zs])) {
+                    query(flip(zs));
+                }
+                vector<bool> xs = select_best(history);
+                cerr << "gacha done:\t\"" << to_hashed(xs, 250) << "\" -> " << history[xs] << endl;
+                xss = unconcat(xs, l);
+            }
         } else {
-            int j = i / (X/(N/l));
+            int j = (query_count - x0) / (X/(N/l));
             vector<vector<bool> > yss = xss;
             yss[j] = random_binary(l);
             int score = query(concat(yss));
